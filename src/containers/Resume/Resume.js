9dc5356f4253e0b098c9dea466dcd8a2412415d6
Shirong Foo <dee.sr.foo@gmail.com>
2017-06-19 12:42:11 +0800

    removed react component in render function

--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -5,9 +5,6 @@ export default class Resume extends Component {
   render () {
     return (
       <div>
-        <ReactPDF
-          file='https://ga-core.s3.amazonaws.com/cms/files/files/000/004/660/original/170601_GA_CodingResume.pdf?utm_medium=email&utm_source=transactional&utm_campaign=2017-Q2-coding-resume-confirmation&utm_content=marketing'
-              />
       </div>
     )
   }
